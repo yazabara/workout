e686b339b5f2a59886d6ad02e828dd06cbe9cdfa
yazabara <yazabara@gmail.com>
2014-11-26 15:22:37 +0300

    Удалил лишний layout

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,10 +4,10 @@ var settings = {
     firebaseUrl: 'https://blinding-inferno-9356.firebaseio.com/'
 };
 
-var myAccountApp = angular.module('myAccountApp', ['firebase', 'AuthService'])
+var workoutPortalApp = angular.module('workoutPortalApp', ['firebase', 'AuthService'])
     .constant('FIRE_BASE_URL', settings.firebaseUrl);
 
-myAccountApp.controller('AccountController', ["$scope", 'AuthCustom',
+workoutPortalApp.controller('AccountController', ["$scope", 'AuthCustom',
     function ($scope, AuthCustom) {
         $scope.facebookAuth = function () {
             AuthCustom.loginRemote('facebook');
