83e11caffb91f1df840fa32e75c5c5764d3e5734
yazabara <yazabara@gmail.com>
2014-12-22 16:40:02 +0300

    Jade structure was fixed;

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,17 +5,8 @@ define(['routes/page'], function (page) {
 		app.get('/:name', page.renderPartials);
 
         //error handlers
-        app.use(function(req, res, next){
-            res.status(404);
-            console.log('Not found URL: %s',req.url);
-            res.render('layouts/layout');
-        });
-
-        app.use(function(err, req, res, next){
-            res.status(err.status || 500);
-            console.log('Internal error(%d): %s',res.statusCode,err.message);
-            res.render('layouts/layout');
-        });
+        app.use(page.error404handler);
+        app.use(page.errorHandler);
 
 	};
 });
--- server/routes/page.js
+++ server/routes/page.js
@@ -2,14 +2,32 @@
  * Показываем основной layout - и его части, если они есть.
  */
 define(['app'], function (app) {
+
+    var mainView = "main";
+
 	function renderIndex(req, res){
-		res.render('layouts/layout');
+		res.render(mainView);
 	}
 	function renderPartials (req, res) {
 		res.render(req.params.name);
 	}
+
+    function error404handler (req, res, next){
+        res.status(404);
+        console.log('Not found URL: %s',req.url);
+        res.render(mainView);
+    }
+
+    function errorHandler (req, res, next){
+        res.status(err.status || 500);
+        console.log('Internal error(%d): %s',res.statusCode,err.message);
+        res.render(mainView);
+    }
+
 	return {
 		renderIndex: renderIndex,
+        error404handler: error404handler,
+        errorHandler : errorHandler,
 		renderPartials: renderPartials
 	};
 });
