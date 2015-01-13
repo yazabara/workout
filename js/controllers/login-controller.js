d24b0f346d54f43428713007b36fa748816d9110
Ildar <KeinAsylum@gmail.com>
2015-01-13 23:39:56 +0300

    added createUser function

--- js/controllers/login-controller.js
+++ js/controllers/login-controller.js
@@ -1,4 +1,5 @@
-workoutPortalApp.controller('LoginController', ['$scope', 'AuthCustom', '$location', function ($scope, AuthCustom, $location) {
+workoutPortalApp.controller('LoginController', ['$scope', 'AuthCustom', '$location', '$log', 'FIRE_BASE_URL', function ($scope, AuthCustom, $location, $log, FIRE_BASE_URL) {
+	var ref = new Firebase(FIRE_BASE_URL);
 	$scope.userEmail = null;
 	$scope.userPassword = null;
 
@@ -7,7 +8,25 @@ workoutPortalApp.controller('LoginController', ['$scope', 'AuthCustom', '$locati
 			email: $scope.userEmail,
 			password: $scope.userPassword
 		}).then(function(authData) {
+			$log.log(authData.uid + " logged in");
 			$location.path("/workout/profile");
+		});
+	};
+
+	$scope.createUser = function() {
+		AuthCustom.$createUser($scope.userEmail, $scope.userPassword).then(function() {
+			AuthCustom.$authWithPassword({
+				email: $scope.userEmail,
+				password: $scope.userPassword
+			}).then(function(authData) {
+				$log.log("Create user: " + authData.uid);
+				ref.child('users').child(authData.uid).set({ email: $scope.userEmail });
+				$location.path("/workout/profile");
+			}).catch(function(error) {
+				$log.error(error);
+			});
+		}).catch(function(error) {
+			$log.error(error);
 		});
 	};
 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -52,7 +52,8 @@ workoutPortalApp.run(["$rootScope", "$location", function($rootScope, $location)
 		}
 	});
 }]);
-workoutPortalApp.controller('LoginController', ['$scope', 'AuthCustom', '$location', function ($scope, AuthCustom, $location) {
+workoutPortalApp.controller('LoginController', ['$scope', 'AuthCustom', '$location', '$log', 'FIRE_BASE_URL', function ($scope, AuthCustom, $location, $log, FIRE_BASE_URL) {
+	var ref = new Firebase(FIRE_BASE_URL);
 	$scope.userEmail = null;
 	$scope.userPassword = null;
 
@@ -61,7 +62,25 @@ workoutPortalApp.controller('LoginController', ['$scope', 'AuthCustom', '$locati
 			email: $scope.userEmail,
 			password: $scope.userPassword
 		}).then(function(authData) {
+			$log.log(authData.uid + " logged in");
 			$location.path("/workout/profile");
+		});
+	};
+
+	$scope.createUser = function() {
+		AuthCustom.$createUser($scope.userEmail, $scope.userPassword).then(function() {
+			AuthCustom.$authWithPassword({
+				email: $scope.userEmail,
+				password: $scope.userPassword
+			}).then(function(authData) {
+				$log.log("Create user: " + authData.uid);
+				ref.child('users').child(authData.uid).set({ email: $scope.userEmail });
+				$location.path("/workout/profile");
+			}).catch(function(error) {
+				$log.error(error);
+			});
+		}).catch(function(error) {
+			$log.error(error);
 		});
 	};
 
